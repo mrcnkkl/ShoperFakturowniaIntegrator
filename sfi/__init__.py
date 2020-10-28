c8c133b4b33c5d343ba3655be571938b65e76a76
Marcin <mrcn.kkl@gmail.com>
2020-10-28 21:14:05 +0100

    added sha1 calculating on webhook load

--- sfi/__init__.py
+++ sfi/__init__.py
@@ -4,12 +4,14 @@ from sfi import forms
 import os
 import hashlib
 
+
 def create_app():
     app = Flask(__name__)
     app.secret_key = os.getenv("SMTP_SERVER") or "very_very_secret_key_to_be_changed_in_production"
 
-    @app.route("/test")
+    @app.route("/test", methods=["GET", "POST"])
     def test():
+        print(request.data.decode("utf-8"))
         return "<h3> ### mrcn ### heroku ### </h3>"
 
     TEMP_TEXT_FILE = "./temp.json"
@@ -17,7 +19,7 @@ def create_app():
     SHA_FILE = "./sha.txt"
 
     def _calculate_webhook_sha(webhook_id, user_secret, webhook_data):
-        load = str.encode(webhook_id)+b":"+str.encode(user_secret)+b":"+str.encode(webhook_data)
+        load = str.encode(webhook_id) + b":" + str.encode(user_secret) + b":" + str.encode(webhook_data)
         return hashlib.sha1(load).hexdigest()
 
     @app.route("/api/webhook", methods=["POST"])
@@ -29,7 +31,8 @@ def create_app():
         with open(HEADERS_FILE, "a+") as file:
             file.write(str(headers))
         with open(SHA_FILE, "w+") as file:
-            file.write(_calculate_webhook_sha("3", "21497ED1A0D3B473004E2A061A12AD2AF28BAAC2E6B3B7CE046C0E17340A3F47", str(request.data)))
+            file.write(_calculate_webhook_sha("3", "21497ED1A0D3B473004E2A061A12AD2AF28BAAC2E6B3B7CE046C0E17340A3F47",
+                                              request.data.decode("utf-8")))
         return jsonify({"status": "OK"})
 
     @app.route("/check_temp", methods=["GET"])
